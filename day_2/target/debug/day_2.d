1a05075d836f20536ee26f30cfae76f925c0123c
Tim Anderegg <timothy.anderegg@gmail.com>
2022-12-02 12:16:31 -0500

    Cleaned up unneeded files.

--- day_2/target/debug/day_2.d
+++
@@ -1,1 +0,0 @@
-/home/tanderegg/advent-of-code-2022/day_2/target/debug/day_2: /home/tanderegg/advent-of-code-2022/day_2/src/main.rs
--- day_2/target/debug/deps/day_2-9fc84a3f84ac3fc7.d
+++
@@ -1,5 +0,0 @@
-/home/tanderegg/advent-of-code-2022/day_2/target/debug/deps/day_2-9fc84a3f84ac3fc7: src/main.rs
-
-/home/tanderegg/advent-of-code-2022/day_2/target/debug/deps/day_2-9fc84a3f84ac3fc7.d: src/main.rs
-
-src/main.rs:
